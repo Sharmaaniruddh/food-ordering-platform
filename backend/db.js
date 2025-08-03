e2f47df9a5357fda4267748bc8645197b9f101e3
Aniruddh Sharma <153709623+Sharmaaniruddh@users.noreply.github.com>
2025-08-04 00:35:30 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,22 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST || "localhost" ,
+//   user: process.env.DB_USER || "postgres",
+//   password: process.env.DB_PASSWORD || "password",
+//   database: process.env.DB_NAME || "foodweb",
+//   port: process.env.DB_PORT || 5432,
+// });
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost" ,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_NAME || "foodweb",
-  port: process.env.DB_PORT || 5432,
+host: process.env.PGHOST,
+user: process.env.PGUSER,
+password: process.env.PGPASSWORD,
+database: process.env.PGDATABASE,
+port: process.env.PGPORT,
+ssl: {
+rejectUnauthorized: false, // required by Render
+},
 });
 
 module.exports = pool;
